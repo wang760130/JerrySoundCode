38ba529a0529e21005e91b68d19693e325f054a6
JerryWang <jerry002@126.com>
2016-02-06 22:43:32 +0800

    init project

---
+++ com.jerry/src/main/java/com/jerry/soundcode/App.java
@@ -0,0 +1,13 @@
+package com.jerry.soundcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
