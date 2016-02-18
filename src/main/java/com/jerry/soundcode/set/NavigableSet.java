877f1262e296f3f55e495860ec790eb53d2a2e0a
wangjiajun <jerry002@126.com>
2016-02-18 15:29:11 +0800

    NavigableSet

---
+++ src/main/java/com/jerry/soundcode/set/NavigableSet.java
@@ -0,0 +1,35 @@
+package com.jerry.soundcode.set;
+
+import com.jerry.soundcode.list.Iterator;
+
+public interface NavigableSet<T> extends SortedSet<T> {
+
+	T lowet(T t);
+	
+	T floor(T t);
+	
+	T ceiling(T t);
+	
+	T higher(T t);
+	
+	T pollFirst();
+	
+	T pollLast();
+	
+	Iterator<T> iterator();
+	
+	NavigableSet<T> descendingSet();
+	
+	Iterator<T> descendingIterator();
+	
+	NavigableSet<T> subSet(T fromElement, boolean fromInclusive,
+			T toElement, boolean toInclusive);
+	
+	NavigableSet<T> headSet(T toElement, boolean inclusive);
+	
+	NavigableSet<T> tailSet(T fromElement, boolean inclusive);
+	
+	SortedSet<T> subSet(T fromElement, T toElement);
+	
+	SortedSet<T> headSet(T toElement);
+}
