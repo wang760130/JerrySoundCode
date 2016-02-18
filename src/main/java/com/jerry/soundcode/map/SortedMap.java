14da088cb6e373008d1f9d286e382da4cd30dcf2
wangjiajun <jerry002@126.com>
2016-02-18 11:31:12 +0800

    SortedMap

---
+++ src/main/java/com/jerry/soundcode/map/SortedMap.java
@@ -0,0 +1,27 @@
+package com.jerry.soundcode.map;
+
+import java.util.Comparator;
+
+import com.jerry.soundcode.list.Collection;
+import com.jerry.soundcode.set.Set;
+
+public interface SortedMap<K, V> extends Map<K, V> {
+	
+	Comparator<? super K> comparator();
+	
+	SortedMap<K, V> subMap(K fromKey, K toKey);
+	
+	SortedMap<K, V> headMap(K toKey);
+	
+	SortedMap<K, V> tailMap(K fromKey);
+	
+	K firstKey();
+	
+	K lastKey();
+	
+	Set<K> keySet();
+	
+	Collection<V> values();
+	
+	Set<Map.Entry<K, V>> entrySet();
+}
