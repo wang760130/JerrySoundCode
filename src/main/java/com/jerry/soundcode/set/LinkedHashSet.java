cced8105c43045c3d65001172a16a39aab0261bc
JerryWang <jerry002@126.com>
2016-02-20 16:40:49 +0800

    LinkedHashSet

--- src/main/java/com/jerry/soundcode/set/LinkedHashSet.java
+++ src/main/java/com/jerry/soundcode/set/LinkedHashSet.java
@@ -2,8 +2,27 @@ package com.jerry.soundcode.set;
 
 import java.io.Serializable;
 
+import com.jerry.soundcode.list.Collection;
+
 public class LinkedHashSet<T> extends HashSet<T> 
 	implements Set<T>, Cloneable, Serializable {
 
+	private static final long serialVersionUID = 1L;
+
+	public LinkedHashSet(int initialCapacity, float loadFactor) {
+		super(initialCapacity, loadFactor, true);
+	}
 	
+	public LinkedHashSet(int initialCapacity) {
+		super(initialCapacity, 0.75f, true);
+	}
+	
+	public LinkedHashSet() {
+		super(16, 0.75f, true);
+	}
+	
+	public LinkedHashSet(Collection<? extends T> c) {
+		super(Math.max(2 * c.size(), 11), 0.75f, true);
+		addAll(c);
+	}
 }
