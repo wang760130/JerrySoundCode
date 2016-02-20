b4be7982b84f2318f4a4c57e6443be238942e4bb
JerryWang <jerry002@126.com>
2016-02-20 17:09:22 +0800

    AtomicInteger

--- src/main/java/com/jerry/soundcode/concurrent/atomic/AtomicInteger.java
+++ src/main/java/com/jerry/soundcode/concurrent/atomic/AtomicInteger.java
@@ -1,7 +1,146 @@
 package com.jerry.soundcode.concurrent.atomic;
 
-public class AtomicInteger {
+import java.io.Serializable;
 
+public class AtomicInteger extends Number implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final Unsafe unsafe = Unsafe.getUnsafe();
 	
+	private static final long valueOffset;
+	
+	static {
+		try {
+			valueOffset = unsafe.objectFieldOffset(AtomicInteger.class.getDeclaredField("value"));
+		} catch (Exception e) {
+			throw new Error(e);
+		} 
+	}
+	
+	private volatile int value;
+	
+	public AtomicInteger(int initialValue) {
+		value = initialValue;
+	}
+	
+	public AtomicInteger() {
+		
+	}
+	
+	public final int get() {
+		return value;
+	}
+	
+	public final void set(int newValue) {
+		value = newValue;
+	}
+	
+	public final void lazySet(int newValue) {
+		unsafe.putOrderedInt(this, valueOffset, newValue);
+	}
+	
+	public final int getAndSet(int newValue) {
+		for(;;) {
+			int current = get();
+			if(compareAndSet(current, newValue)) {
+				return current;
+			}
+		}
+	}
+	
+	public final boolean compareAndSet(int expect, int update) {
+		return unsafe.compareAndSwapInt(this, valueOffset, expect, update);
+	}
+	
+	public final boolean weakCompareAndSet(int expect, int update) {
+		return unsafe.compareAndSwapInt(this, valueOffset, expect, update);
+	}
+	
+	public final int getAndIncrement() {
+		for(;;) {
+			int current = get();
+			int next = current + 1;
+			if(compareAndSet(current, next)) {
+				return current;
+			}
+		}
+	}
+	
+	public final int getAndDecrement() {
+		for(;;) {
+			int current = get();
+			int next = current - 1;
+			if(compareAndSet(current, next)) {
+				return current;
+			}
+		}
+	}
+	
+	public final int getAndAdd(int delta) {
+		for(;;) {
+			int current = get();
+			int next = current + delta;
+			if(compareAndSet(current, next)) {
+				return current;
+			}
+		}
+	}
+	
+	public final int incrementAndGet() {
+		for(;;) {
+			int current = get();
+			int next = current + 1;	
+			if(compareAndSet(current, next)) {
+				return next;
+			}
+		}
+	}
+	
+	public final int decrementAndGet() {
+		for(;;) {
+			int current = get();
+			int next = current - 1;
+			if(compareAndSet(current, next)) {
+				return next;
+			}
+		}
+	}
+	
+	public final int addAndGet(int delta) {
+		for(;;) {
+			int current = get();
+			int next = current + delta;
+			if(compareAndSet(current, next)) {
+				return next;
+			}
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return Integer.toString(get());
+	}
+	
+	
+	@Override
+	public int intValue() {
+		return get();
+	}
+
+	@Override
+	public long longValue() {
+		return (long)get();
+	}
+
+	@Override
+	public float floatValue() {
+		return (float)get();
+	}
+
+	@Override
+	public double doubleValue() {
+		return (double)get();
+	}
 
 }
