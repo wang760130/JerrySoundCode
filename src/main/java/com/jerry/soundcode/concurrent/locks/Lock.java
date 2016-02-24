741d15dc7f75339208b5157ccca46261f09999c1
JerryWang <jerry002@126.com>
2016-02-24 23:09:55 +0800

    Lock

---
+++ src/main/java/com/jerry/soundcode/concurrent/locks/Lock.java
@@ -0,0 +1,18 @@
+package com.jerry.soundcode.concurrent.locks;
+
+import java.util.concurrent.TimeUnit;
+
+public interface Lock {
+
+	void lock();
+	
+	void lockInterruptibly() throws InterruptedException;
+	
+	boolean tryLock();
+	
+	boolean tryLock(long time, TimeUnit unit) throws InterruptedException;
+	
+	void unlock();
+	
+	Condition newCOndition();
+}
