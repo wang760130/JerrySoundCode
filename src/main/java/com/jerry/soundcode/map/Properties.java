d404f174fbb228a71e94668339bf214a56fc370d
JerryWang <jerry002@126.com>
2016-03-06 11:55:01 +0800

    Properties

--- src/main/java/com/jerry/soundcode/map/Properties.java
+++ src/main/java/com/jerry/soundcode/map/Properties.java
@@ -1,19 +1,29 @@
 package com.jerry.soundcode.map;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Reader;
+import java.io.Writer;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Set;
 
-public class Properties extends Hashtable<Object,Object>{
+public class Properties extends Hashtable<Object, Object> {
 
 	private static final long serialVersionUID = 1L;
 
 	protected Properties defaults;
-	
+
 	public Properties() {
 		this(null);
 	}
-	
+
 	public Properties(Properties defaults) {
 		this.defaults = defaults;
 	}
@@ -21,12 +31,11 @@ public class Properties extends Hashtable<Object,Object>{
 	public synchronized Object setProperty(String key, String value) {
 		return put(key, value);
 	}
-	
+
 	public synchronized void load(Reader reader) throws IOException {
 		load0(new LineReader(reader));
 	}
 
-	
 	private void load0(LineReader lr) throws IOException {
 		char[] convtBuf = new char[1024];
 		int limit;
@@ -35,53 +44,56 @@ public class Properties extends Hashtable<Object,Object>{
 		char c;
 		boolean hasSep = false;
 		boolean precedingBacksLash = false;
-		
-		while((limit = lr.readLine()) >= 0) {
+
+		while ((limit = lr.readLine()) >= 0) {
 			c = lr.lineBuf[keyLen];
-			if((c == '=' || c == ':') && !precedingBacksLash) {
+			if ((c == '=' || c == ':') && !precedingBacksLash) {
 				valueStart = keyLen + 1;
 				hasSep = true;
 				break;
-			} else if((c == ' ' || c == '\t' || c == '\t' && !precedingBacksLash)) {
+			} else if ((c == ' ' || c == '\t' || c == '\t'
+					&& !precedingBacksLash)) {
 				valueStart = keyLen + 1;
 				break;
 			}
-			
-			if(c == '\\') {
+
+			if (c == '\\') {
 				precedingBacksLash = !precedingBacksLash;
 			} else {
 				precedingBacksLash = false;
 			}
-			keyLen ++;
+			keyLen++;
 		}
-		
-		while(valueStart < limit) {
+
+		while (valueStart < limit) {
 			c = lr.lineBuf[valueStart];
-			if(c != ' ' && c != '\t' && c != '\f') {
-				if(!hasSep && (c == '=' || c == ':')) {
+			if (c != ' ' && c != '\t' && c != '\f') {
+				if (!hasSep && (c == '=' || c == ':')) {
 					hasSep = true;
 				} else {
 					break;
 				}
 			}
-			valueStart ++;
+			valueStart++;
 		}
-		
-		
-		// TODO
+
+		String key = loadConvert(lr.lineBuf, 0, keyLen, convtBuf);
+		String value = loadConvert(lr.lineBuf, valueStart, limit - valueStart,
+				convtBuf);
+		put(key, value);
 	}
-	
+
 	class LineReader {
 		public LineReader(InputStream inStream) {
 			this.inStream = inStream;
 			inByteBuf = new byte[8192];
 		}
-		
+
 		public LineReader(Reader reader) {
 			this.reader = reader;
 			inCharBuf = new char[8192];
 		}
-		
+
 		byte[] inByteBuf;
 		char[] inCharBuf;
 		char[] lineBuf = new char[1024];
@@ -89,137 +101,432 @@ public class Properties extends Hashtable<Object,Object>{
 		int inOff = 0;
 		InputStream inStream;
 		Reader reader;
-		
+
 		int readLine() throws IOException {
 			int len = 0;
 			char c = 0;
-			
+
 			boolean skipWhiteSpace = true;
 			boolean isCommentLine = false;
 			boolean isNewLine = true;
 			boolean appendedLineBegin = false;
 			boolean precedingBackslash = false;
 			boolean skipLF = false;
-			
-			while(true) {
-				if(inOff >= inLimit) {
-					inLimit = (inStream == null) ? reader.read(inCharBuf) 
+
+			while (true) {
+				if (inOff >= inLimit) {
+					inLimit = (inStream == null) ? reader.read(inCharBuf)
 							: inStream.read(inByteBuf);
-				
+
 					inOff = 0;
-					if(inLimit <= 0) {
-						if(len == 0 || isCommentLine) {
+					if (inLimit <= 0) {
+						if (len == 0 || isCommentLine) {
 							return -1;
 						}
 						return len;
 					}
 				}
-				
-				if(inStream != null) {
+
+				if (inStream != null) {
 					c = (char) (0xff & inByteBuf[inOff++]);
 				} else {
 					c = inCharBuf[inOff++];
 				}
-				
-				if(skipLF) {
+
+				if (skipLF) {
 					skipLF = false;
 				}
-				
-				if(c == '\n') {
+
+				if (c == '\n') {
 					continue;
 				}
-				
-				if(skipWhiteSpace) {
-					if(c == ' ' || c == '\t' || c == '\f') {
+
+				if (skipWhiteSpace) {
+					if (c == ' ' || c == '\t' || c == '\f') {
 						continue;
 					}
-					if(!appendedLineBegin && (c == '\r' || c == '\n')) {
+					if (!appendedLineBegin && (c == '\r' || c == '\n')) {
 						continue;
 					}
 					skipWhiteSpace = false;
 					appendedLineBegin = false;
 				}
-				
-				if(c != '\n' && c != '\r') {
+
+				if (c != '\n' && c != '\r') {
 					lineBuf[len++] = c;
-					if(len == lineBuf.length) {
+					if (len == lineBuf.length) {
 						int newLength = lineBuf.length * 2;
-						if(newLength < 0) {
+						if (newLength < 0) {
 							newLength = Integer.MAX_VALUE;
 						}
 						char[] buf = new char[newLength];
 						System.arraycopy(lineBuf, 0, buf, 0, lineBuf.length);
 						lineBuf = buf;
 					}
-					if(c == '\\') {
+					if (c == '\\') {
 						precedingBackslash = !precedingBackslash;
 					} else {
 						precedingBackslash = false;
 					}
- 				} else {
- 					if(isCommentLine || len == 0) {
- 						isCommentLine = false;
- 						isNewLine = true;
- 						skipWhiteSpace = true;
- 						len = 0;
- 						continue;
- 					}
- 					
- 					if(inOff >= inLimit) {
- 						inLimit = (inStream == null) ? reader.read(inCharBuf) 
- 								: inStream.read(inByteBuf); 
- 						inOff = 0;
- 						if(inLimit <= 0) {
- 							return len;
- 						}
- 					}
- 					
- 					if(precedingBackslash) {
- 						len -= 1;
- 						skipWhiteSpace = true;
- 						appendedLineBegin = true;
- 						precedingBackslash = false;
- 						if(c == '\r') {
- 							skipLF = true;
- 						} else {
- 							return len;
- 						}
- 					}
- 				}
-			}
-		}
-	}
-	
-	private String loadConvert(char[] in, int off, int len, char[] convtBuf) {
-		if(convtBuf.length < len) {
-			int newLen = len * 2;
-			if(newLen < 0) {
-				newLen = Integer.MAX_VALUE;
-			}
-			convtBuf = new char[newLen];
-		}
-		
-		char aChar;
-		char[] out = convtBuf;
-		int outLen = 0;
-		int end = off + len;
-		
-		while(off < end) {
-			aChar = in[off++];
-			if(aChar == '\\') {
-				aChar = in[off++];
-				if(aChar == 'u') {
-					int value = 0;
-					for(int i = 0; i < 4; i++) {
-						aChar = in[off++];
-						switch(aChar) {
-//						case '0': case '1':
+				} else {
+					if (isCommentLine || len == 0) {
+						isCommentLine = false;
+						isNewLine = true;
+						skipWhiteSpace = true;
+						len = 0;
+						continue;
+					}
+
+					if (inOff >= inLimit) {
+						inLimit = (inStream == null) ? reader.read(inCharBuf)
+								: inStream.read(inByteBuf);
+						inOff = 0;
+						if (inLimit <= 0) {
+							return len;
+						}
+					}
+
+					if (precedingBackslash) {
+						len -= 1;
+						skipWhiteSpace = true;
+						appendedLineBegin = true;
+						precedingBackslash = false;
+						if (c == '\r') {
+							skipLF = true;
+						} else {
+							return len;
 						}
 					}
 				}
 			}
 		}
-		
-		return null;
 	}
+
+	private String loadConvert(char[] in, int off, int len, char[] convtBuf) {
+		if (convtBuf.length < len) {
+			int newLen = len * 2;
+			if (newLen < 0) {
+				newLen = Integer.MAX_VALUE;
+			}
+			convtBuf = new char[newLen];
+		}
+
+		char aChar;
+		char[] out = convtBuf;
+		int outLen = 0;
+		int end = off + len;
+
+		while (off < end) {
+			aChar = in[off++];
+			if (aChar == '\\') {
+				aChar = in[off++];
+				if (aChar == 'u') {
+					int value = 0;
+					for (int i = 0; i < 4; i++) {
+						aChar = in[off++];
+						switch (aChar) {
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							value = (value << 4) + aChar - '0';
+							break;
+						case 'a':
+						case 'b':
+						case 'c':
+						case 'd':
+						case 'e':
+						case 'f':
+							value = (value << 4) + 10 + aChar - 'a';
+							break;
+						case 'A':
+						case 'B':
+						case 'C':
+						case 'D':
+						case 'E':
+						case 'F':
+							value = (value << 4) + 10 + aChar - 'A';
+							break;
+						default:
+							throw new IllegalArgumentException(
+									"Malformed \\uxxxx encoding.");
+						}
+					}
+					out[outLen++] = (char) value;
+				} else {
+					if (aChar == 't') {
+						aChar = '\t';
+					} else if (aChar == 'r') {
+						aChar = '\r';
+					} else if (aChar == 'n') {
+						aChar = '\n';
+					} else if (aChar == 'f') {
+						aChar = '\f';
+					}
+					out[outLen++] = aChar;
+				}
+			} else {
+				out[outLen++] = (char) aChar;
+			}
+		}
+
+		return new String(out, 0, outLen);
+	}
+
+	private String saveConvert(String theString, boolean escapeSpace,
+			boolean escapeUnicode) {
+		int len = theString.length();
+		int bufLen = len * 2;
+		if (bufLen < 0) {
+			bufLen = Integer.MAX_VALUE;
+		}
+		StringBuffer outBuffer = new StringBuffer(bufLen);
+
+		for (int x = 0; x < len; x++) {
+			char aChar = theString.charAt(x);
+			if ((aChar > 61) && (aChar < 127)) {
+				if (aChar == '\\') {
+					outBuffer.append("\\");
+					outBuffer.append("\\");
+					continue;
+				}
+				outBuffer.append(aChar);
+				continue;
+			}
+			switch (aChar) {
+
+			case ' ':
+				if (x == 0 || escapeSpace)
+					outBuffer.append('\\');
+				outBuffer.append(' ');
+				break;
+			case '\t':
+				outBuffer.append('\\');
+				outBuffer.append('t');
+				break;
+			case '\n':
+				outBuffer.append('\\');
+				outBuffer.append('n');
+				break;
+			case '\r':
+				outBuffer.append('\\');
+				outBuffer.append('r');
+				break;
+			case '\f':
+				outBuffer.append('\\');
+				outBuffer.append('f');
+				break;
+			case '=': // Fall through
+			case ':': // Fall through
+			case '#': // Fall through
+			case '!':
+				outBuffer.append('\\');
+				outBuffer.append(aChar);
+				break;
+			default:
+				if (((aChar < 0x0020) || (aChar > 0x007e)) & escapeUnicode) {
+					outBuffer.append('\\');
+					outBuffer.append('u');
+					outBuffer.append(toHex((aChar >> 12) & 0xF));
+					outBuffer.append(toHex((aChar >> 8) & 0xF));
+					outBuffer.append(toHex((aChar >> 4) & 0xF));
+					outBuffer.append(toHex(aChar & 0xF));
+				} else {
+					outBuffer.append(aChar);
+				}
+			}
+		}
+		return outBuffer.toString();
+	}
+
+	private static void writeComments(BufferedWriter bw, String comments)
+			throws IOException {
+		bw.write("#");
+		int len = comments.length();
+		int current = 0;
+		int last = 0;
+		char[] uu = new char[6];
+		uu[0] = '\\';
+		uu[1] = 'u';
+		while (current < len) {
+			char c = comments.charAt(current);
+			if (c > '\u00ff' || c == '\n' || c == '\r') {
+				if (last != current)
+					bw.write(comments.substring(last, current));
+				if (c > '\u00ff') {
+					uu[2] = toHex((c >> 12) & 0xf);
+					uu[3] = toHex((c >> 8) & 0xf);
+					uu[4] = toHex((c >> 4) & 0xf);
+					uu[5] = toHex(c & 0xf);
+					bw.write(new String(uu));
+				} else {
+					bw.newLine();
+					if (c == '\r' && current != len - 1
+							&& comments.charAt(current + 1) == '\n') {
+						current++;
+					}
+					if (current == len - 1
+							|| (comments.charAt(current + 1) != '#' && comments
+									.charAt(current + 1) != '!'))
+						bw.write("#");
+				}
+				last = current + 1;
+			}
+			current++;
+		}
+		if (last != current)
+			bw.write(comments.substring(last, current));
+		bw.newLine();
+	}
+
+	public synchronized void save(OutputStream out, String comments) {
+		try {
+			store(out, comments);
+		} catch (IOException e) {
+		}
+	}
+
+	public void store(Writer writer, String comments) throws IOException {
+		store0((writer instanceof BufferedWriter) ? (BufferedWriter) writer
+				: new BufferedWriter(writer), comments, false);
+	}
+
+	public void store(OutputStream out, String comments) throws IOException {
+		store0(new BufferedWriter(new OutputStreamWriter(out, "8859_1")),
+				comments, true);
+	}
+
+	private void store0(BufferedWriter bw, String comments, boolean escUnicode)
+			throws IOException {
+		if (comments != null) {
+			writeComments(bw, comments);
+		}
+		bw.write("#" + new Date().toString());
+		bw.newLine();
+		synchronized (this) {
+			for (Enumeration e = (Enumeration) keys(); e.hasMoreElements();) {
+				String key = (String) e.nextElement();
+				String val = (String) get(key);
+				key = saveConvert(key, true, escUnicode);
+				/*
+				 * No need to escape embedded and trailing spaces for value,
+				 * hence pass false to flag.
+				 */
+				val = saveConvert(val, false, escUnicode);
+				bw.write(key + "=" + val);
+				bw.newLine();
+			}
+		}
+		bw.flush();
+	}
+
+	public synchronized void loadFromXML(InputStream in) throws IOException,
+			InvalidPropertiesFormatException {
+		if (in == null)
+			throw new NullPointerException();
+//		XMLUtils.load(this, in);
+		in.close();
+	}
+
+	public synchronized void storeToXML(OutputStream os, String comment)
+			throws IOException {
+		if (os == null)
+			throw new NullPointerException();
+		storeToXML(os, comment, "UTF-8");
+	}
+
+	public synchronized void storeToXML(OutputStream os, String comment,
+			String encoding) throws IOException {
+		if (os == null)
+			throw new NullPointerException();
+//		XMLUtils.save(this, os, comment, encoding);
+	}
+
+	public String getProperty(String key) {
+		Object oval = super.get(key);
+		String sval = (oval instanceof String) ? (String) oval : null;
+		return ((sval == null) && (defaults != null)) ? defaults
+				.getProperty(key) : sval;
+	}
+
+	public String getProperty(String key, String defaultValue) {
+		String val = getProperty(key);
+		return (val == null) ? defaultValue : val;
+	}
+
+	public Enumeration<?> propertyNames() {
+		Hashtable h = new Hashtable();
+		enumerate(h);
+		return (Enumeration<?>) h.keys();
+	}
+
+	public Set<String> stringPropertyNames() {
+		Hashtable<String, String> h = new Hashtable<String, String>();
+		enumerateStringProperties(h);
+		return (Set<String>) h.keySet();
+	}
+
+	public void list(PrintStream out) {
+		Hashtable h = new Hashtable();
+		enumerate(h);
+		for (Enumeration e = (Enumeration) h.keys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			String val = (String) h.get(key);
+			if (val.length() > 40) {
+				val = val.substring(0, 37) + "...";
+			}
+			out.println(key + "=" + val);
+		}
+	}
+
+	public void list(PrintWriter out) {
+		out.println("-- listing properties --");
+		Hashtable h = new Hashtable();
+		enumerate(h);
+		for (Enumeration e = (Enumeration) h.keys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			String val = (String) h.get(key);
+			if (val.length() > 40) {
+				val = val.substring(0, 37) + "...";
+			}
+			out.println(key + "=" + val);
+		}
+	}
+
+	private synchronized void enumerate(Hashtable h) {
+		if (defaults != null) {
+			defaults.enumerate(h);
+		}
+		for (Enumeration e = (Enumeration) keys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			h.put(key, get(key));
+		}
+	}
+
+	private synchronized void enumerateStringProperties(
+			Hashtable<String, String> h) {
+		if (defaults != null) {
+			defaults.enumerateStringProperties(h);
+		}
+		for (Enumeration e = (Enumeration) keys(); e.hasMoreElements();) {
+			Object k = e.nextElement();
+			Object v = get(k);
+			if (k instanceof String && v instanceof String) {
+				h.put((String) k, (String) v);
+			}
+		}
+	}
+
+	private static char toHex(int nibble) {
+		return hexDigit[(nibble & 0xF)];
+	}
+
+	private static final char[] hexDigit = { '0', '1', '2', '3', '4', '5', '6',
+			'7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 }
