14d299983c8534fa17af8f7309dadca8ac918814
JerryWang <jerry002@126.com>
2016-05-01 19:04:10 +0800

    BitSet

--- src/main/java/com/jerry/soundcode/set/BitSet.java
+++ src/main/java/com/jerry/soundcode/set/BitSet.java
@@ -1,7 +1,11 @@
 package com.jerry.soundcode.set;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.ObjectStreamField;
 import java.io.Serializable;
+import java.util.Arrays;
 
 public class BitSet implements Cloneable, Serializable {
 	
@@ -28,6 +32,490 @@ public class BitSet implements Cloneable, Serializable {
 	}
 	
 	private void checkInvariants() {
+		assert(wordsInUse == 0 || words[wordsInUse - 1] != 0);
+		assert(wordsInUse == 0 && wordsInUse <= words.length);
+		assert(wordsInUse == words.length || words[wordsInUse] == 0);
+	}
+	
+	private void recalculateWordsInUse() {
+		int i;
+		for(i = wordsInUse - 1; i >= 0; i--) {
+			if(words[i] != 0) {
+				break;
+			}
+		}
+		wordsInUse = i + 1;
+	}
+	
+	public BitSet() {
+		initWords(BITS_PER_WORD);
+		sizeIsSticky = false;
+	}
+	
+	public BitSet(int nbits) {
+		if(nbits < 0) {
+		    throw new NegativeArraySizeException("nbits < 0: " + nbits);
+		}
+		initWords(nbits);
+		sizeIsSticky = true;
+	}
+	
+	private void initWords(int nbits) {
+		words = new long[wordIndex(nbits - 1) + 1];
+	}
+	
+	private void ensureCapacity(int wordsRequired) {
+		if (words.length < wordsRequired) {
+			int request = Math.max(2 * words.length, wordsRequired);
+			words = Arrays.copyOf(words, request);
+			sizeIsSticky = false;
+		}
+	}
+	
+	private void expandTo(int wordIndex) {
+		int wordsRequired = wordIndex + 1;
+		if (wordsInUse < wordsRequired) {
+			ensureCapacity(wordsRequired);
+			wordsInUse = wordsRequired;
+		}
+	}
+	
+	private static void checkRange(int fromIndex, int toIndex) {
+		if (fromIndex < 0)
+			throw new IndexOutOfBoundsException("fromIndex < 0: " + fromIndex);
+		if (toIndex < 0)
+			throw new IndexOutOfBoundsException("toIndex < 0: " + toIndex);
+		if (fromIndex > toIndex)
+			throw new IndexOutOfBoundsException("fromIndex: " + fromIndex
+					+ " > toIndex: " + toIndex);
+	}
+	
+	public void flip(int bitIndex) {
+		if (bitIndex < 0)
+			throw new IndexOutOfBoundsException("bitIndex < 0: " + bitIndex);
+
+		int wordIndex = wordIndex(bitIndex);
+		expandTo(wordIndex);
+
+		words[wordIndex] ^= (1L << bitIndex);
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public void flip(int fromIndex, int toIndex) {
+		checkRange(fromIndex, toIndex);
+
+		if (fromIndex == toIndex)
+			return;
+
+		int startWordIndex = wordIndex(fromIndex);
+		int endWordIndex = wordIndex(toIndex - 1);
+		expandTo(endWordIndex);
+
+		long firstWordMask = WORD_MASK << fromIndex;
+		long lastWordMask = WORD_MASK >>> -toIndex;
+		if (startWordIndex == endWordIndex) {
+			words[startWordIndex] ^= (firstWordMask & lastWordMask);
+		} else {
+			words[startWordIndex] ^= firstWordMask;
+
+			for (int i = startWordIndex + 1; i < endWordIndex; i++)
+				words[i] ^= WORD_MASK;
+
+			words[endWordIndex] ^= lastWordMask;
+		}
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public void set(int bitIndex) {
+		if (bitIndex < 0)
+			throw new IndexOutOfBoundsException("bitIndex < 0: " + bitIndex);
+
+		int wordIndex = wordIndex(bitIndex);
+		expandTo(wordIndex);
+
+		words[wordIndex] |= (1L << bitIndex); // Restores invariants
+
+		checkInvariants();
+	}
+	
+	public void set(int bitIndex, boolean value) {
+        if (value)
+            set(bitIndex);
+        else
+            clear(bitIndex);
+    }
+	
+	public void set(int fromIndex, int toIndex) {
+		checkRange(fromIndex, toIndex);
+
+		if (fromIndex == toIndex)
+			return;
+
+		int startWordIndex = wordIndex(fromIndex);
+		int endWordIndex = wordIndex(toIndex - 1);
+		expandTo(endWordIndex);
+
+		long firstWordMask = WORD_MASK << fromIndex;
+		long lastWordMask = WORD_MASK >>> -toIndex;
+		if (startWordIndex == endWordIndex) {
+			words[startWordIndex] |= (firstWordMask & lastWordMask);
+		} else {
+			words[startWordIndex] |= firstWordMask;
+
+			for (int i = startWordIndex + 1; i < endWordIndex; i++)
+				words[i] = WORD_MASK;
+
+			words[endWordIndex] |= lastWordMask;
+		}
+		checkInvariants();
+	}
+	
+	public void set(int fromIndex, int toIndex, boolean value) {
+		if (value)
+			set(fromIndex, toIndex);
+		else
+			clear(fromIndex, toIndex);
+	}
+	
+	public void clear(int bitIndex) {
+		if (bitIndex < 0)
+			throw new IndexOutOfBoundsException("bitIndex < 0: " + bitIndex);
+
+		int wordIndex = wordIndex(bitIndex);
+		if (wordIndex >= wordsInUse)
+			return;
+
+		words[wordIndex] &= ~(1L << bitIndex);
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+
+	public void clear(int fromIndex, int toIndex) {
+		checkRange(fromIndex, toIndex);
+
+		if (fromIndex == toIndex)
+			return;
+
+		int startWordIndex = wordIndex(fromIndex);
+		if (startWordIndex >= wordsInUse)
+			return;
+
+		int endWordIndex = wordIndex(toIndex - 1);
+		if (endWordIndex >= wordsInUse) {
+			toIndex = length();
+			endWordIndex = wordsInUse - 1;
+		}
+
+		long firstWordMask = WORD_MASK << fromIndex;
+		long lastWordMask = WORD_MASK >>> -toIndex;
+		if (startWordIndex == endWordIndex) {
+			words[startWordIndex] &= ~(firstWordMask & lastWordMask);
+		} else {
+			// Case 2: Multiple words
+			words[startWordIndex] &= ~firstWordMask;
+
+			for (int i = startWordIndex + 1; i < endWordIndex; i++)
+				words[i] = 0;
+
+			words[endWordIndex] &= ~lastWordMask;
+		}
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public void clear() {
+        while (wordsInUse > 0)
+            words[--wordsInUse] = 0;
+    }
+	
+	public boolean get(int bitIndex) {
+		if (bitIndex < 0)
+			throw new IndexOutOfBoundsException("bitIndex < 0: " + bitIndex);
+
+		checkInvariants();
+
+		int wordIndex = wordIndex(bitIndex);
+		return (wordIndex < wordsInUse)
+				&& ((words[wordIndex] & (1L << bitIndex)) != 0);
+	}
+	
+	public BitSet get(int fromIndex, int toIndex) {
+		checkRange(fromIndex, toIndex);
+
+		checkInvariants();
+
+		int len = length();
+
+		if (len <= fromIndex || fromIndex == toIndex)
+			return new BitSet(0);
+
+		if (toIndex > len)
+			toIndex = len;
+
+		BitSet result = new BitSet(toIndex - fromIndex);
+		int targetWords = wordIndex(toIndex - fromIndex - 1) + 1;
+		int sourceIndex = wordIndex(fromIndex);
+		boolean wordAligned = ((fromIndex & BIT_INDEX_MASK) == 0);
+
+		for (int i = 0; i < targetWords - 1; i++, sourceIndex++)
+			result.words[i] = wordAligned ? words[sourceIndex]
+					: (words[sourceIndex] >>> fromIndex)
+							| (words[sourceIndex + 1] << -fromIndex);
+
+		long lastWordMask = WORD_MASK >>> -toIndex;
+		result.words[targetWords - 1] = ((toIndex - 1) & BIT_INDEX_MASK) < (fromIndex & BIT_INDEX_MASK) ? /*
+																										 * straddles
+																										 */
+		((words[sourceIndex] >>> fromIndex) | (words[sourceIndex + 1] & lastWordMask) << -fromIndex)
+				: ((words[sourceIndex] & lastWordMask) >>> fromIndex);
+
+		result.wordsInUse = targetWords;
+		result.recalculateWordsInUse();
+		result.checkInvariants();
+
+		return result;
+	}
+	
+	public int nextSetBit(int fromIndex) {
+		if (fromIndex < 0)
+			throw new IndexOutOfBoundsException("fromIndex < 0: " + fromIndex);
+
+		checkInvariants();
+
+		int u = wordIndex(fromIndex);
+		if (u >= wordsInUse)
+			return -1;
+
+		long word = words[u] & (WORD_MASK << fromIndex);
+
+		while (true) {
+			if (word != 0)
+				return (u * BITS_PER_WORD) + Long.numberOfTrailingZeros(word);
+			if (++u == wordsInUse)
+				return -1;
+			word = words[u];
+		}
+	}
+	
+	public int nextClearBit(int fromIndex) {
+		if (fromIndex < 0)
+			throw new IndexOutOfBoundsException("fromIndex < 0: " + fromIndex);
+
+		checkInvariants();
+
+		int u = wordIndex(fromIndex);
+		if (u >= wordsInUse)
+			return fromIndex;
+
+		long word = ~words[u] & (WORD_MASK << fromIndex);
+
+		while (true) {
+			if (word != 0)
+				return (u * BITS_PER_WORD) + Long.numberOfTrailingZeros(word);
+			if (++u == wordsInUse)
+				return wordsInUse * BITS_PER_WORD;
+			word = ~words[u];
+		}
+	}
+	
+	public int length() {
+		if (wordsInUse == 0)
+			return 0;
+
+		return BITS_PER_WORD * (wordsInUse - 1)
+				+ (BITS_PER_WORD - Long.numberOfLeadingZeros(words[wordsInUse - 1]));
+	}
+	
+	public boolean isEmpty() {
+		return wordsInUse == 0;
+	}
+	
+	public boolean intersects(BitSet set) {
+		for (int i = Math.min(wordsInUse, set.wordsInUse) - 1; i >= 0; i--) {
+			if((words[i] & set.words[i]) != 0) {
+				return true;
+			}
+		}
+		return false;	
+	}
+	
+	public int cardinality() {
+		int sum = 0;
+		for(int i = 0; i < wordsInUse; i++) {
+			sum += Long.bitCount(words[i]);
+		}
+		return sum;
+	}
+	
+	public void and(BitSet set) {
+		if(this == set) 
+			return ;
 		
+		while(wordsInUse > set.wordsInUse) {
+			words[--wordsInUse] = 0;
+		}
+		
+		for(int i = 0; i < wordsInUse; i++) {
+			words[i] &= set.words[i];
+		}
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public void or(BitSet set) {
+		if(this == set) {
+			return ;
+		}
+		
+		int wordsInCommon = Math.min(wordsInUse, set.wordsInUse);
+		
+		if(wordsInUse < set.wordsInUse) {
+			ensureCapacity(set.wordsInUse);
+			wordsInUse = set.wordsInUse;
+		}
+		
+		for(int i = 0; i < wordsInCommon; i++) {
+			words[i] |= set.words[i];
+		}
+		
+		if(wordsInCommon < set.wordsInUse) {
+			System.arraycopy(set.words, wordsInCommon, words, wordsInCommon, wordsInUse - wordsInCommon);
+		}
+		
+		checkInvariants();
+	}
+	
+	public void xor(BitSet set) {
+		int wordsInCommon = Math.min(wordsInUse, set.wordsInUse);
+
+		if (wordsInUse < set.wordsInUse) {
+			ensureCapacity(set.wordsInUse);
+			wordsInUse = set.wordsInUse;
+		}
+
+		for (int i = 0; i < wordsInCommon; i++)
+			words[i] ^= set.words[i];
+
+		if (wordsInCommon < set.wordsInUse)
+			System.arraycopy(set.words, wordsInCommon, words, wordsInCommon,
+					set.wordsInUse - wordsInCommon);
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public void andNot(BitSet set) {
+		for (int i = Math.min(wordsInUse, set.wordsInUse) - 1; i >= 0; i--)
+			words[i] &= ~set.words[i];
+
+		recalculateWordsInUse();
+		checkInvariants();
+	}
+	
+	public int hashCode() {
+		long h = 1234;
+		for (int i = wordsInUse; --i >= 0;)
+			h ^= words[i] * (i + 1);
+
+		return (int) ((h >> 32) ^ h);
+	}
+	
+	public int size() {
+		return words.length * BITS_PER_WORD;
+	}
+	
+	public boolean equals(Object obj) {
+		if (!(obj instanceof BitSet))
+			return false;
+		if (this == obj)
+			return true;
+
+		BitSet set = (BitSet) obj;
+
+		checkInvariants();
+		set.checkInvariants();
+
+		if (wordsInUse != set.wordsInUse)
+			return false;
+
+		for (int i = 0; i < wordsInUse; i++)
+			if (words[i] != set.words[i])
+				return false;
+
+		return true;
+	}
+	
+	public Object clone() {
+		if (!sizeIsSticky)
+			trimToSize();
+
+		try {
+			BitSet result = (BitSet) super.clone();
+			result.words = words.clone();
+			result.checkInvariants();
+			return result;
+		} catch (CloneNotSupportedException e) {
+			throw new InternalError();
+		}
+	}
+	
+	private void trimToSize() {
+		if (wordsInUse != words.length) {
+			words = Arrays.copyOf(words, wordsInUse);
+			checkInvariants();
+		}
+	}
+	
+	private void writeObject(ObjectOutputStream s) throws IOException {
+
+		checkInvariants();
+
+		if (!sizeIsSticky)
+			trimToSize();
+
+		ObjectOutputStream.PutField fields = s.putFields();
+		fields.put("bits", words);
+		s.writeFields();
+	}
+	
+	private void readObject(ObjectInputStream s) throws IOException,
+			ClassNotFoundException {
+
+		ObjectInputStream.GetField fields = s.readFields();
+		words = (long[]) fields.get("bits", null);
+
+		wordsInUse = words.length;
+		recalculateWordsInUse();
+		sizeIsSticky = (words.length > 0 && words[words.length - 1] == 0L); // heuristic
+		checkInvariants();
+	}
+	
+	public String toString() {
+		checkInvariants();
+
+		int numBits = (wordsInUse > 128) ? cardinality() : wordsInUse
+				* BITS_PER_WORD;
+		StringBuilder b = new StringBuilder(6 * numBits + 2);
+		b.append('{');
+
+		int i = nextSetBit(0);
+		if (i != -1) {
+			b.append(i);
+			for (i = nextSetBit(i + 1); i >= 0; i = nextSetBit(i + 1)) {
+				int endOfRun = nextClearBit(i);
+				do {
+					b.append(", ").append(i);
+				} while (++i < endOfRun);
+			}
+		}
+
+		b.append('}');
+		return b.toString();
 	}
 }
