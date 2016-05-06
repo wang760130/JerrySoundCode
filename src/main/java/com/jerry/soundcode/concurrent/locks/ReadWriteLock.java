2da94c9505828daa93c1e868b304a4b2b98cc8e9
wangjiajun <jerry002@126.com>
2016-05-06 17:25:44 +0800

    lock

--- src/main/java/com/jerry/soundcode/concurrent/locks/ReadWriteLock.java
+++ src/main/java/com/jerry/soundcode/concurrent/locks/ReadWriteLock.java
@@ -1,5 +1,9 @@
 package com.jerry.soundcode.concurrent.locks;
 
+/**
+ * 读写锁拆成读锁和写锁来理解。读锁可以共享，多个线程可以同时拥有读锁，但是写锁却只能只有一个线程拥有，
+ * 而且获取写锁的时候其他线程都已经释放了读锁，而且该线程获取写锁之后，其他线程不能再获取读锁。简单的说就是写锁是排他锁，读锁是共享锁。 
+ */
 public interface ReadWriteLock {
 	
 	Lock readLock();
