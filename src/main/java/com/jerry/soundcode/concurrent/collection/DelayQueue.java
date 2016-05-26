bebbb9025fca248c71e4b557cebdb508378339ae
JerryWang <jerry002@126.com>
2016-05-26 23:28:32 +0800

    BlockingDeque

---
+++ src/main/java/com/jerry/soundcode/concurrent/collection/DelayQueue.java
@@ -0,0 +1,10 @@
+package com.jerry.soundcode.concurrent.collection;
+
+/**
+ * 一个无界阻塞队列，只有在延迟期满时才能从中提取元素。
+ * 该队列的头部是延迟期满后保存时间最长的Delayed 元素。
+ * 根据这个特性么我们可以使用DelayQueue来实现缓存系统、实时调度系统等。
+ */
+public class DelayQueue {
+
+}
