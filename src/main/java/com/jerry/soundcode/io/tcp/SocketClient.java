3bda005d348524c44c927bdf886f50dad4d73916
wangjiajun <jerry002@126.com>
2016-06-07 11:41:45 +0800

    tcp

--- src/main/java/com/jerry/soundcode/io/tcp/SocketClient.java
+++ src/main/java/com/jerry/soundcode/io/tcp/SocketClient.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -13,37 +14,50 @@ public class SocketClient {
 	String host = "localhost";
 	int port = 8888;
 	
+	Socket socket = null;
+	OutputStream os = null;
+	PrintWriter pw = null;
+	InputStream is = null;
+	InputStreamReader reader = null;
+	BufferedReader buffer = null;
+	
 	public void sendServer() {
 		
 		try {
 			// 创建Socket对象（并连接服务器）
-			Socket socket = new Socket(host, port);
+			socket = new Socket(host, port);
 			
 			// 调用getOutputStream方法，进行I/O
-			PrintWriter pw = new PrintWriter(socket.getOutputStream());
+			os = socket.getOutputStream();
+			pw = new PrintWriter(os);
 			
 			// 向服务器发送消息
 			pw.println("Hello World, this is client message");
 			pw.flush();
 			
 			// 接收服务器返回的数据
-			InputStream is = socket.getInputStream();
-			InputStreamReader reader = new InputStreamReader(is);
-			BufferedReader buffer = new BufferedReader(reader);
+			is = socket.getInputStream();
+			reader = new InputStreamReader(is);
+			buffer = new BufferedReader(reader);
 			
 			String line = buffer.readLine();
-			System.out.println("Server return message:" + line);
-			
-			is.close();
-			reader.close();
-			buffer.close();
-			pw.close();
-			socket.close();
+			System.out.println("Server return message : " + line);
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+				reader.close();
+				buffer.close();
+				os.close();
+				pw.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
--- src/main/java/com/jerry/soundcode/io/tcp/SocketServer.java
+++ src/main/java/com/jerry/soundcode/io/tcp/SocketServer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,40 +13,54 @@ public class SocketServer {
 	
 	int port = 8888;
 	
+	ServerSocket serverSocket = null;
+	Socket socket = null;
+	InputStream in = null;
+	InputStreamReader reader = null;
+	BufferedReader buffer = null;
+	OutputStream os = null;
+	PrintWriter pw = null;
+	
 	public void startServer() {
 		try {
 			// 创建ServerSocket对象（并绑定端口）
-			ServerSocket serverSocket = new ServerSocket(port);
+			serverSocket = new ServerSocket(port);
 			
-			/**
-			 * 调用accept方法，接收客户端的数据
-			 * 对于accept方法的调用将造成阻塞，直到ServerSocket接受到一个连接请求为止。
-			 * 一旦连接请求被接受，服务器可以读客户socket中的请求。
-			 */
-			Socket socket = serverSocket.accept();
-			
-			InputStream in = socket.getInputStream();
-			InputStreamReader reader = new InputStreamReader(in);
-			BufferedReader buffer = new BufferedReader(reader);
-			
-			// 输出客户端传过来的数据
-			System.out.println("Accept client data:");
-			String line = buffer.readLine();
-			System.out.println(line);
-			
-			// 给客户端传输数据
-			PrintWriter pw = new PrintWriter(socket.getOutputStream());
-			pw.println(line + " from server");
-			pw.flush();
-			
-			pw.close();
-			buffer.close();
-			reader.close();
-			in.close();
-			socket.close();
-			serverSocket.close();
+			// 调用accept方法，接收客户端的数据
+			// 对于accept方法的调用将造成阻塞，直到ServerSocket接受到一个连接请求为止。
+			// 一旦连接请求被接受，服务器可以读客户socket中的请求。
+			while(true) {
+				socket = serverSocket.accept();
+				
+				in = socket.getInputStream();
+				reader = new InputStreamReader(in);
+				buffer = new BufferedReader(reader);
+				
+				// 输出客户端传过来的数据
+				System.out.println("Accept client data : ");
+				String line = buffer.readLine();
+				System.out.println(line);
+				
+				// 给客户端传输数据
+				os = socket.getOutputStream();
+				pw = new PrintWriter(socket.getOutputStream());
+				pw.println(line + " from server");
+				pw.flush();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				pw.close();
+				os.close();
+				buffer.close();
+				reader.close();
+				in.close();
+				socket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
