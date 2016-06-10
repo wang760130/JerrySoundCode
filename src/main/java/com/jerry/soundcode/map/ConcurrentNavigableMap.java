d80f8e325a49e107ae9d3a1e949e0bc6f84db51b
JerryWang <jerry002@126.com>
2016-06-10 18:45:06 +0800

    ConcurrentNavigableMap

---
+++ src/main/java/com/jerry/soundcode/map/ConcurrentNavigableMap.java
@@ -0,0 +1,29 @@
+package com.jerry.soundcode.map;
+
+import com.jerry.soundcode.concurrent.collection.ConcurrentMap;
+import com.jerry.soundcode.set.NavigableSet;
+
+public interface ConcurrentNavigableMap<K, V> 
+	extends ConcurrentMap<K, V>, NavigableMap<K, V> {
+
+	ConcurrentNavigableMap<K, V> subMap(K fromKey, boolean fromInclusive, K toKey, boolean toInclusive);
+	
+	ConcurrentNavigableMap<K, V> headMap(K toKey, boolean inclusive);
+	
+	ConcurrentNavigableMap<K, V> tailMap(K fromKey, boolean inclusive);
+	
+	ConcurrentNavigableMap<K, V> subMap(K fromKey, K toKey);
+	
+	ConcurrentNavigableMap<K, V> headMap(K toKey);
+	
+	ConcurrentNavigableMap<K, V> tailMap(K fromKey);
+	
+	ConcurrentNavigableMap<K, V> descendingMap();
+	
+	public NavigableSet<K> navigableKeySet();
+	
+	NavigableSet<K> keySet();
+	
+	public NavigableSet<K> descendingKeySet();
+	
+}
