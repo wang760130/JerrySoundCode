ff5d2d4d41baf782e24e5aea05e7411f25a1ad36
JerryWang <jerry002@126.com>
2016-06-23 22:31:40 +0800

    CountDownLatch

--- src/main/java/com/jerry/soundcode/thread/CountDownLatch.java
+++ src/main/java/com/jerry/soundcode/thread/CountDownLatch.java
@@ -4,6 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import com.jerry.soundcode.concurrent.locks.AbstractQueuedSynchronizer;
 
+/**
+ * CountDownLatch这个类能够使一个线程等待其他线程完成各自的工作后再执行。例如，应用程序的主线程希望在负责启动框架服务的线程已经启动所有的框架服务之后再执行。
+ * CountDownLatch是通过一个计数器来实现的，计数器的初始值为线程的数量。每当一个线程完成了自己的任务后，计数器的值就会减1。
+ * 当计数器值到达0时，它表示所有的线程已经完成了任务，然后在闭锁上等待的线程就可以恢复执行任务。
+ */
 public class CountDownLatch {
 
 	private static final class Sync extends AbstractQueuedSynchronizer {
