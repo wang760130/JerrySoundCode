49a063a6320aa743ba7927566b6998b0d797206e
JerryWang <jerry002@126.com>
2016-06-26 20:13:43 +0800

    CompletionService

---
+++ src/main/java/com/jerry/soundcode/thread/CompletionService.java
@@ -0,0 +1,17 @@
+package com.jerry.soundcode.thread;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public interface CompletionService<V> {
+	
+	Future<V> submit(Callable<V> task);
+	
+	Future<V> submit(Runnable task, V result);
+	
+	Future<V> task() throws InterruptedException;
+	
+	Future<V> poll();
+	
+	Future<V> poll(long timeout, TimeUnit unit) throws InterruptedException;
+}
