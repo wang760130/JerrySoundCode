b65ce607bf9ab865e56c7e145a156ba0553e9051
JerryWang <jerry002@126.com>
2016-06-27 01:01:26 +0800

    Executors

--- src/main/java/com/jerry/soundcode/thread/Executors.java
+++ src/main/java/com/jerry/soundcode/thread/Executors.java
@@ -1,9 +1,14 @@
 package com.jerry.soundcode.thread;
 
+
 public class Executors {
 
 	public Executors() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public static <T> Callable<T> callable(Runnable task, T result) {
+		return null;
+	
+	}
 }
