548c8c9f7b57436b2f3a4219df85dae8f1d34425
JerryWang <jerry002@126.com>
2016-06-26 23:02:00 +0800

    Future

---
+++ src/main/java/com/jerry/soundcode/thread/Future.java
@@ -0,0 +1,19 @@
+package com.jerry.soundcode.thread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public interface Future<V> {
+	
+	boolean cancel(boolean mayInterruptIfRunning);
+	
+	boolean isCancelled();
+	
+	boolean isDone();
+	
+	V get() throws InterruptedException, ExecutionException;
+	
+	V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException;
+	
+}
