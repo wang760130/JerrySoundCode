7398d0d82c8949c2393edfbc1cbd9b88ae3b9fd1
JerryWang <jerry002@126.com>
2016-06-27 23:47:11 +0800

    ReentrantReadWriteLock

--- src/main/java/com/jerry/soundcode/concurrent/locks/ReentrantReadWriteLock.java
+++ src/main/java/com/jerry/soundcode/concurrent/locks/ReentrantReadWriteLock.java
@@ -7,6 +7,19 @@ import java.util.concurrent.TimeUnit;
 import com.jerry.soundcode.thread.Thread;
 import com.jerry.soundcode.list.Collection;
 
+/**
+ * 读写锁：分为读锁和写锁，多个读锁不互斥，读锁与写锁互斥，这是由jvm自己控制的，你只要上好相应的锁即可。
+ * 如果你的代码只读数据，可以很多人同时读，但不能同时写，那就上读锁；如果你的代码修改数据，只能有一个人在写，且不能同时读取，那就上写锁。
+ * 总之，读的时候上读锁，写的时候上写锁！
+ * 
+ * ReentrantReadWriteLock会使用两把锁来解决问题，一个读锁，一个写锁
+ * 线程进入读锁的前提条件：
+ * 没有其他线程的写锁，
+ * 没有写请求或者有写请求，但调用线程和持有锁的线程是同一个
+ * 线程进入写锁的前提条件：
+ * 没有其他线程的读锁
+ * 没有其他线程的写锁
+ */
 public class ReentrantReadWriteLock implements ReadWriteLock, Serializable{
 
 	private static final long serialVersionUID = 1L;
