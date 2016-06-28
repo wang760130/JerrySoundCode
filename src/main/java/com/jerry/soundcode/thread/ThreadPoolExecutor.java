b32eae6ab5b4b92bddf9e3a2b90f896b93640251
JerryWang <jerry002@126.com>
2016-06-28 23:30:41 +0800

    ThreadPoolExecutor

--- src/main/java/com/jerry/soundcode/thread/ThreadPoolExecutor.java
+++ src/main/java/com/jerry/soundcode/thread/ThreadPoolExecutor.java
@@ -1,9 +1,46 @@
 package com.jerry.soundcode.thread;
 
-public class ThreadPoolExecutor {
+import java.util.concurrent.TimeUnit;
 
-	public ThreadPoolExecutor() {
-		// TODO Auto-generated constructor stub
+import com.jerry.soundcode.list.List;
+
+public class ThreadPoolExecutor extends AbstractExecutorService {
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Runnable> shutdownNow() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isShutdown() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isTerminated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean awaitTermination(long timeout, TimeUnit unit)
+			throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void execute(Runnable command) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
