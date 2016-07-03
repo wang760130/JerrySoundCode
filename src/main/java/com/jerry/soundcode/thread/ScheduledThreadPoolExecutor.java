5e977f51d893074c53b7be29a4f4ad9c3bf7a0f9
JerryWang <jerry002@126.com>
2016-07-03 22:29:17 +0800

    ScheduledThreadPoolExecutor

--- src/main/java/com/jerry/soundcode/thread/ScheduledThreadPoolExecutor.java
+++ src/main/java/com/jerry/soundcode/thread/ScheduledThreadPoolExecutor.java
@@ -1,12 +1,14 @@
 package com.jerry.soundcode.thread;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import com.jerry.soundcode.concurrent.atomic.AtomicLong;
+import com.jerry.soundcode.concurrent.collection.BlockingQueue;
 import com.jerry.soundcode.concurrent.collection.DelayQueue;
 import com.jerry.soundcode.concurrent.collection.Delayed;
+import com.jerry.soundcode.list.AbstractCollection;
 import com.jerry.soundcode.list.Collection;
+import com.jerry.soundcode.list.Iterator;
 import com.jerry.soundcode.list.List;
 
 public class ScheduledThreadPoolExecutor extends ThreadPoolExecutor implements ScheduledExecutorService {
@@ -115,13 +117,13 @@ public class ScheduledThreadPoolExecutor extends ThreadPoolExecutor implements S
     		return ;
     	}
     	
-    	if(getPoolSize() < getPoolSize()) {
+    	if(getPoolSize() < getCorePoolSize()) {
     		prestartCoreThread();
     	}
     	super.getQueue().add(command);
     }
     
-    private void cancelUnwantedTasks() {
+	private void cancelUnwantedTasks() {
     	boolean keepDelayed = getExecuteExistingDelayedTasksAfterShutdownPolicy();
     	boolean keepPeriodic = getContinueExistingPeriodicTasksAfterShutdownPolicy();
     	
@@ -159,149 +161,280 @@ public class ScheduledThreadPoolExecutor extends ThreadPoolExecutor implements S
     	return task;
     }
     
-    public ScheduledThreadPoolExecutor(int corePoolSize) {
+    @SuppressWarnings("rawtypes")
+	public ScheduledThreadPoolExecutor(int corePoolSize) {
     	super(corePoolSize, Integer.MAX_VALUE, 0, TimeUnit.NANOSECONDS, new DelayQueue());
     }
     
+    public ScheduledThreadPoolExecutor(int corePoolSize, ThreadFactory threadFactory) {
+    	super(corePoolSize, Integer.MAX_VALUE, 0, TimeUnit.NANOSECONDS, new DelayedWorkQueue(), threadFactory);
+    }
     
+    public ScheduledThreadPoolExecutor(int corePoolSize, RejectedExecutionHandler handler) {
+    	super(corePoolSize, Integer.MAX_VALUE, 0, TimeUnit.NANOSECONDS, new DelayedWorkQueue(), handler);
+    }
     
-	private void purge() {
-		
+    private long triggerTime(long delay, TimeUnit unit) {
+    	return triggerTime(unit.toNanos((delay < 0) ? 0 : delay));
+    }
+    
+    long triggerTime(long delay) {
+    	return now() + ((delay < (Long.MAX_VALUE >> 1)) ? delay : overflowFree(delay));
+    }
+    
+	private long overflowFree(long delay) {
+		Delayed head = (Delayed) super.getQueue().peek();
+		if(head != null) {
+			long headDelay = head.getDelay(TimeUnit.NANOSECONDS);
+			if(headDelay < 0 && (delay - headDelay < 0)) {
+				delay = Long.MAX_VALUE + headDelay;
+			}
+		}
+		return delay;
 	}
-
-	private boolean getExecuteExistingDelayedTasksAfterShutdownPolicy() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public ScheduledThreadPoolExecutor(int corePoolSize, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+		super(corePoolSize, Integer.MAX_VALUE, 0, TimeUnit.NANOSECONDS, new DelayedWorkQueue(), threadFactory, handler);
 	}
-
-	private void prestartCoreThread() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private int getPoolSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private void reject(Runnable command) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void shutdown() {
-//		boolean ok = ScheduledFutureTask.super.runAndReset();
-	}
-
 	
 	@Override
-	public List<Runnable> shutdownNow() {
-		// TODO Auto-generated method stub
-		return null;
+	public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+		if(command == null || unit == null) {
+			throw new NullPointerException();
+		}
+		RunnableScheduledFuture<?> t = decorateTask(command, new ScheduledFutureTask<Void>(command, null, triggerTime(delay, unit)));
+		delayedExecute(t);
+		return t;
 	}
-
+	
 	@Override
-	public boolean isShutdown() {
-		// TODO Auto-generated method stub
-		return false;
+	public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+		if(callable == null || unit == null) {
+			throw new NullPointerException();
+		}
+		
+		RunnableScheduledFuture<V> t = decorateTask(callable, new ScheduledFutureTask<V>(callable, triggerTime(delay, unit)));
+		delayedExecute(t);
+		return t;
 	}
-
+	
 	@Override
-	public boolean isTerminated() {
-		// TODO Auto-generated method stub
-		return false;
+	public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, int initialDelay, long period, TimeUnit unit) {
+		if(command == null || unit == null) {
+			throw new NullPointerException();
+		}
+		
+		if(period <= 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		RunnableScheduledFuture<?> t = decorateTask(command, new ScheduledFutureTask<Object>(command, null, triggerTime(initialDelay, unit), unit.toNanos(period)));
+		delayedExecute(t);
+		return t;
 	}
-
-	@Override
-	public boolean awaitTermination(long timeout, TimeUnit unit)
-			throws InterruptedException {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+		if(command == null || unit == null) {
+			throw new NullPointerException();
+		}
+		if(delay < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		RunnableScheduledFuture<?> t = decorateTask(command, new ScheduledFutureTask<Boolean> (command, null, triggerTime(initialDelay, unit), unit.toNanos(-delay)));
+		delayedExecute(t);
+		return t;	
 	}
-
-	@Override
-	public <T> Future<T> submit(Callable<T> task) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> Future<T> submit(Runnable tsk, T result) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Future<?> submit(Runnable task) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
-			throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> List<Future<T>> invokeAll(
-			Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-			throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
-			throws InterruptedException, ExecutionException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <T> T invokeAny(Collection<? extends Callable<T>> tasks,
-			long timeout, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 	@Override
 	public void execute(Runnable command) {
-		// TODO Auto-generated method stub
-		
+		if(command == null) {
+			throw new NullPointerException();
+		}
+		schedule(command, 0, TimeUnit.NANOSECONDS);
 	}
-
+	
 	@Override
-	public ScheduledFuture<?> schedule(Runnable command, long delay,
-			TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
+	public Future<?> submit(Runnable task) {
+		return schedule(task, 0, TimeUnit.NANOSECONDS);
 	}
-
+	
 	@Override
-	public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay,
-			TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
+	public <T> Future<T> submit(Runnable task, T result) {
+		return schedule(Executors.callable(task, result), 0, TimeUnit.NANOSECONDS);
 	}
-
+	
 	@Override
-	public ScheduledFuture<?> scheduleAtFixedRate(Runnable command,
-			int initialDelay, long period, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
+	public <T> Future<T> submit(Callable<T> task) {
+		return schedule(task, 0, TimeUnit.NANOSECONDS);
 	}
-
-	@Override
-	public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command,
-			long initialDelay, long delay, TimeUnit unit) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public void setContinueExistingPeriodicTasksAfterShutdownPolicy(
+			boolean value) {
+		this.continueExistingPeriodicTasksAfterShutdown = value;
+		if(!value && isShutdown()) {
+			cancelUnwantedTasks();
+		}
 	}
 
 	public boolean getContinueExistingPeriodicTasksAfterShutdownPolicy() {
 		return continueExistingPeriodicTasksAfterShutdown;
+	}
+	
+	public void setExecuteExistingDelayedTasksAfterShutdownPolicy(boolean value) {
+        executeExistingDelayedTasksAfterShutdown = value;
+        if (!value && isShutdown())
+            cancelUnwantedTasks();
+    }
+	
+	public boolean getExecuteExistingDelayedTasksAfterShutdownPolicy() {
+        return executeExistingDelayedTasksAfterShutdown;
+    }
+	
+	@Override
+	public void shutdown() {
+		cancelUnwantedTasks();
+		super.shutdown();
+	}
+	
+	@Override
+	public List<Runnable> shutdownNow() {
+		return super.shutdownNow();
+	}
+
+	public BlockingQueue<Runnable> getQueue() {
+		return super.getQueue();
+	}
+	
+	private static class DelayedWorkQueue extends AbstractCollection<Runnable> implements BlockingQueue<Runnable> {
+		
+		@SuppressWarnings("rawtypes")
+		private final DelayQueue<RunnableScheduledFuture> dp = new DelayQueue<RunnableScheduledFuture>();
+		
+		@Override
+		public Runnable poll() {
+			return dp.poll();
+		}
+		
+		@Override
+		public Runnable peek() {
+			return dp.peek();
+		}
+		
+		@Override
+		public Runnable take() throws InterruptedException {
+			return dp.take();
+		}
+		
+		@Override
+		public Runnable poll(long timeout, TimeUnit unit) throws InterruptedException {
+			return dp.poll(timeout, unit);
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@Override
+		public boolean add(Runnable x) {
+			return dp.add((RunnableScheduledFuture)x);
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@Override
+		public boolean offer(Runnable x) {
+			return dp.offer((RunnableScheduledFuture)x);
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@Override
+		public void put(Runnable x) {
+			dp.put((RunnableScheduledFuture)x);
+		}
+
+		@SuppressWarnings("rawtypes")
+		@Override
+		public boolean offer(Runnable x, long timeout, TimeUnit unit) {
+			return dp.offer((RunnableScheduledFuture)x, timeout, unit);
+		}
+		
+		@Override
+		public Runnable remove() {
+			return dp.remove();
+		}
+
+		@Override
+		public Runnable element() {
+			return dp.element();
+		}
+
+		@Override
+		public void clear() {
+			dp.clear();
+		}
+		
+		@Override
+		public int drainTo(Collection<? super Runnable> c) {
+			return dp.drainTo(c);
+		}
+		
+		@Override
+		public int drainTo(Collection<? super Runnable> c, int maxElements) {
+			return dp.drainTo(c, maxElements);
+		}
+		
+		@Override
+		public int remainingCapacity() {
+			return dp.remainingCapacity();
+		}
+		
+		@Override
+		public boolean remove(Object x) {
+			return dp.remove(x);
+		}
+		
+		@Override
+		public boolean contains(Object x) {
+			return dp.contains(x);
+		}
+		
+		@Override
+		public int size() {
+			return dp.size();
+		}
+		
+		@Override
+		public boolean isEmpty() {
+			return dp.isEmpty();
+		}
+		
+		@Override
+		public Object[] toArray() {
+			return dp.toArray();
+		}
+		
+		@Override
+		public <T> T[] toArray(T[] array) {
+			return dp.toArray(array);
+		}
+		
+		@Override
+		public Iterator<Runnable> iterator() {
+            return new Iterator<Runnable>() {
+//                private Iterator<RunnableScheduledFuture> it = dq.iterator();
+                
+                public boolean hasNext() { 
+//                	return it.hasNext(); 
+                	return false;
+                }
+                
+                public Runnable next() { 
+//                	return it.next(); 
+                	return null;
+                }
+                
+                public void remove() { 
+//                	it.remove(); 
+                }
+            };
+        }
+
 	}
 }
