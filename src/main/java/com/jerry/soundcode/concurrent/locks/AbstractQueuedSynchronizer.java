145c98757b6ffd8a3e57e6917ed8620b898583ca
JerryWang <jerry002@126.com>
2016-07-26 23:56:19 +0800

    AbstractQueuedSynchronizer

--- src/main/java/com/jerry/soundcode/concurrent/locks/AbstractQueuedSynchronizer.java
+++ src/main/java/com/jerry/soundcode/concurrent/locks/AbstractQueuedSynchronizer.java
@@ -8,6 +8,11 @@ import com.jerry.soundcode.concurrent.atomic.Unsafe;
 import com.jerry.soundcode.list.ArrayList;
 import com.jerry.soundcode.list.Collection;
 
+/**
+ * 提供了一个基于FIFO队列，可以用于构建锁或者其他相关同步装置的基础框架。
+ * 该同步器（以下简称同步器）利用了一个int来表示状态，期望它能够成为实现大部分同步需求的基础。使用的方法是继承，
+ * 子类通过继承同步器并需要实现它的方法来管理其状态，管理的方式就是通过类似acquire和release的方式来操纵状态。
+ */
 public abstract class AbstractQueuedSynchronizer extends AbstractOwnableSynchronizer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
